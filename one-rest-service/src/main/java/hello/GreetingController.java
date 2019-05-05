ffaf09079710120b891e0acb835684bf9e197a12
aishwaryashilpi <aishwaryashilpi@192.168.0.102>
2019-05-05 18:40:00 +0530

    trying weddini-throttling in spring boot

--- one-rest-service/src/main/java/hello/GreetingController.java
+++ one-rest-service/src/main/java/hello/GreetingController.java
@@ -1,12 +1,17 @@
 package hello;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.util.concurrent.RateLimiter;
+import com.weddini.throttling.Throttling;
+import com.weddini.throttling.ThrottlingType;
 
 @RestController
 public class GreetingController {
@@ -20,6 +25,19 @@ public class GreetingController {
     	rateLimiter.acquire();
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+    
+    /**
+     * Throttling configuration:
+     * <p>
+     * allow 3 HTTP GET requests per minute
+     * for each unique {@code javax.servlet.http.HttpServletRequest#getRemoteAddr()}
+     */
+
+    @GetMapping("/throttledController")
+    @Throttling(limit = 3, timeUnit = TimeUnit.MINUTES, type = ThrottlingType.RemoteAddr)
+    public ResponseEntity<String> controllerThrottling() {
+        return ResponseEntity.ok().body("ok");
     }
 
 }
